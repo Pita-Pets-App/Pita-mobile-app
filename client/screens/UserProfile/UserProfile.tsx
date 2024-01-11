c874f0530083e8d5f36023df5a1e10d11843c03f
Oussema Cherif <ousseemacherif@gmail.com>
2024-01-11 16:56:57 +0100

    user profile 40% done

--- client/screens/UserProfile/UserProfile.tsx
+++ client/screens/UserProfile/UserProfile.tsx
@@ -1,21 +1,29 @@
-import React from 'react';
-import { ScrollView, View, Text,StyleSheet ,Dimensions} from 'react-native';
+import React from "react";
+import {
+  ScrollView,
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  Image,
+} from "react-native";
+import UserPets from "./component/UserPets";
+import UserInfo from "./component/UserInfo";
 
-const { width, height } = Dimensions.get('screen')
+const { width, height } = Dimensions.get("screen");
 const UserProfile: React.FC = () => {
-    return (
-        <ScrollView>
-            <View style={styles.UsersProfile}>
-                <Text>ggggghgg</Text>
-            </View>
-        </ScrollView>
-    );
+  return (
+    <ScrollView>
+      <View style={styles.UsersProfile}>
+        <UserInfo />
+        <UserPets />
+      </View>
+    </ScrollView>
+  );
 };
 const styles = StyleSheet.create({
-    UsersProfile: {
-   backgroundColor:"white",
-       
-       
-    },})
+  UsersProfile: {
+    display: "flex",
+  },
+});
 export default UserProfile;
-
---
+++ client/screens/UserProfile/component/UserInfo.tsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from 'react';
+import { ScrollView, View, Text,StyleSheet ,Dimensions,Image,TouchableOpacity} from 'react-native';
+import addPet from '../../../assets/add-pet.png'
+import dog1 from '../../../assets/ownerdog2.png'
+import dog2 from '../../../assets/ownerdog1.png'
+import peticon from '../../../assets/peticon.png'
+import axios from 'axios';
+
+
+
+const { width, height } = Dimensions.get('screen')
+const UserInfo: React.FC = () => {
+    const [hhh,setHhh]=useState([])
+    const getData=async()=>{
+        const result=await axios.get("http://localhost:3000/api/users/1")
+        setHhh(result.data);
+
+        
+    }
+
+    useEffect(()=>{
+        
+        getData()
+    },[])
+    console.log(hhh);
+    
+    return (
+        <View style={styles.petContainer}>
+            <View style={styles.myPets}>
+            <Image style={{width:width*0.1,height:height*0.045}} source={peticon}></Image>
+            <Text style={{fontWeight:'bold',fontSize: 23}}>My Pets</Text>
+            </View>
+        <View style={{display:"flex",flexDirection:'row', gap:20,  marginVertical:15}}>
+        <TouchableOpacity><Image style={styles.PetsImage} source={dog1}></Image></TouchableOpacity>
+        <TouchableOpacity><Image style={styles.PetsImage} source={dog2}></Image></TouchableOpacity>
+        <TouchableOpacity><Image style={styles.PetsImage} source={addPet}></Image></TouchableOpacity>
+        </View>
+        </View>
+    );
+};
+const styles = StyleSheet.create({
+    PetsImage: {
+        width:width*0.22,
+        height:height*0.1,
+        borderRadius:15
+       
+       
+    },
+    myPets : {
+        display:"flex",
+        alignItems:"center",
+        justifyContent:"flex-start",
+        flexDirection:"row",
+        gap:10,
+       
+    },
+    petContainer :{
+        backgroundColor:"white",
+        marginVertical:7,
+        marginHorizontal:20,
+        borderRadius:20,
+        padding:10,
+    }
+
+})
+export default UserInfo;
+
