0fa0a71b9831906f1175baaecf24dc0bf8295d1b
Oussema Cherif <ousseemacherif@gmail.com>
2024-01-17 11:05:37 +0100

    all queries of chat done

--- server/routes/chat.routes.js
+++ server/routes/chat.routes.js
@@ -4,9 +4,7 @@ const {GetConv,getAllRomsOfUser,Addmsg}=require('../controllers/chat.controller'
 
 ChatRoute.put('/Chat/:user',GetConv)
 ChatRoute.put('/Rooms/:id',getAllRomsOfUser)
-// ChatRoute.get('/LFA/:id',OneLFA)
 ChatRoute.post('/Chat',Addmsg)
-// ChatRoute.put('/LFA/:id',UpdateLFA)
-// ChatRoute.delete('/LFA/:id',DeleteLFA)
+
 
 module.exports=ChatRoute
