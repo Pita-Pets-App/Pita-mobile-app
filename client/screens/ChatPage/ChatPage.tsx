c8b9f18a1f0b113f289580a02a6142a9862b3eb5
Oussema Cherif <ousseemacherif@gmail.com>
2024-01-17 17:51:55 +0100

    msg getted

--- client/screens/ChatPage/ChatPage.tsx
+++ client/screens/ChatPage/ChatPage.tsx
@@ -1,16 +1,55 @@
-import React from 'react';
-import { ScrollView, View, Text, StyleSheet, Dimensions, Image } from 'react-native';
+import React,{useState,useEffect} from 'react';
+import { ScrollView, View, Text, StyleSheet, Dimensions, Image, FlatList } from 'react-native';
 import avatar  from "../../assets/user.jpg"
+import axios from 'axios';
+import { port } from '../../port';
 
 const {width,height} = Dimensions.get('screen')
 
 
 
 const ChatPage: React.FC = (): React.ReactElement => {
+    const [conv, setConv] = useState([]);
+
+    const getData = async () => {
+      try {
+        const result = await axios.put(`${port}/api/Chat/2`,{reciver:3});
+        console.log(result.data);
+        setConv(result.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
   
+    useEffect(() => {
+      getData();
+    }, []);
+
+    const renderConv=({item})=>(
+        
+        <View>
+            {(item.user1==2&&item.msg)?<View>
+        <View style={{justifyContent:"center",alignItems:"flex-start",flexDirection:"column",backgroundColor:"orange", padding:10,width:width*0.5,borderRadius:20,height:height*0.05}}>
+            <Text>{item.msg}</Text>
+
+        </View>
+<Text   style={{fontSize:12,color:"grey"}}>12:55 pm</Text>
+</View>:item.msg?<View style={{marginLeft:width*0.4,justifyContent:"center",alignItems:"flex-start",flexDirection:"column",backgroundColor:"pink", padding:10,width:width*0.5,borderRadius:20,height:height*0.05}}>
+            <Text>{item.msg}</Text>
+
+        </View>:"" }
+           
+        </View> 
+        
+    )
+
     return (
         <ScrollView  style={{flex:1,width:width,height:height,padding:5}}>
-        <View style={styles.avatarContainer}>
+            <FlatList
+            data={conv}
+            renderItem={renderConv}
+            />
+        <View >
             <Image    style={styles.avatar} source={avatar}/>
             <Image  style={styles.avatar} source={avatar}/>
 
@@ -19,15 +58,7 @@ const ChatPage: React.FC = (): React.ReactElement => {
             
 
        
-        <View style={{justifyContent:"center",alignItems:"flex-start",flexDirection:"column",backgroundColor:"orange", padding:10,width:width*0.5,borderRadius:20,height:height*0.05}}>
-            <Text>Hey how are you been</Text>
-
-        </View> 
-        <View style={{justifyContent:"center",alignItems:"flex-start",flexDirection:"column",backgroundColor:"orange", padding:10,width:width*0.5,borderRadius:20,height:height*0.05}}>
-            <Text>wanna to catch drink</Text>
-
-        </View>
-<Text   style={{fontSize:12,color:"grey"}}>12:55 pm</Text>
+      
         <View style={{marginLeft:width*0.4,justifyContent:"center",alignItems:"flex-start",flexDirection:"column",backgroundColor:"pink", padding:10,width:width*0.5,borderRadius:20,height:height*0.05}}>
             <Text>Hey how are you been</Text>
 
