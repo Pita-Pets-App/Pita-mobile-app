256c9c70b4f2bd0dbbf635c5d21db980221b3144
Oussema Cherif <ousseemacherif@gmail.com>
2024-01-17 13:17:18 +0100

    socket.io server side setup

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const cors = require('cors')
+const { Server } = require("socket.io");
+const { createServer } = require("http")
 
 const db = require('./database-Sequelize');
 const usersRoute = require("./routes/users.routes");
@@ -37,8 +39,27 @@ app.use("/api",eventRoute)
 app.use("/api",serviceRoute)
 app.use("/api",ChatRoute)
 
-
+////// chat part 
+const server = createServer(app);
+const io = new Server(server, {
+  cors:{
+    origin:"http://localhost:3000",
+    methods:["GET","POST"]
+  },
+  });
+ io.on("connection",(socket)=>{
+   console.log("Socket Connected" ,socket);
+   socket.on("user connected:",socket.id)
+   socket.on("disconnect", () => {
+    console.log("user disconnected:", socket.id);
+  });
+ })
 
 app.listen(PORT, function () {
   console.log("listening on port 3000!");
 });
+
+server.listen(3001,()=>{
+  console.log("Socket.io is running on port 3001");
+}
+)
