76c833b003b667973f626d7b7b458993386c0a17
Oussema Cherif <ousseemacherif@gmail.com>
2024-01-18 17:23:44 +0100

    some changes

--- client/screens/ChatPage/ChatPage.tsx
+++ client/screens/ChatPage/ChatPage.tsx
@@ -23,7 +23,6 @@ const ChatPage: React.FC = ({route}): React.ReactElement => {
     const getData = async () => {
       try {
         const result = await axios.put(`${port}/api/Chat/2`,{reciver:receiver});
-        console.log(result.data);
         setConv(result.data);
       } catch (error) {
         console.error("Error fetching data:", error);
