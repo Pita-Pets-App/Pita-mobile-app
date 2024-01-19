df79c0f5fe1321d627fe8cc1f7a14a23ea235a9a
Oussema Cherif <ousseemacherif@gmail.com>
2024-01-19 12:06:03 +0100

    some changes done

--- client/screens/PetsProfiles/components/PetsPhoto.tsx
+++ client/screens/PetsProfiles/components/PetsPhoto.tsx
@@ -69,7 +69,7 @@ const styles = StyleSheet.create({
   right: 0,
   shadowColor: '#000',
   shadowOffset: { width: 0, height: 2 },
-  shadowOpacity: 0.2,
+  shadowOpacity: 0.2 ,
   shadowRadius: 4,
   elevation: 4,
   }
