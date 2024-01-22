f5e90ac31f92a0f5f748967fcf62ffc68823edbd
bassdmammar@gmail.com <bassdmammar@gmail.com>
2024-01-22 08:49:04 +0100

    commit

--- admin/src/Components/AllServices.jsx
+++ admin/src/Components/AllServices.jsx
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import Edit from "./EditProfile";
 import styled from "styled-components";
+import Cookies from "js-cookie";
 
 const DeleteButton = styled.button`
   background-color: #ff5a5f;
@@ -10,17 +13,45 @@ const DeleteButton = styled.button`
   border-radius: 5px;
   cursor: pointer;
 `;
+
 function DashboardOverview(props) {
   const [selectedService, setSelectedService] = useState([]);
   const [selectedData, setSelectedData] = useState(null);
   const [selecteduser, setSelecteduser] = useState(null);
   const [selectedprov, setSelectedprov] = useState([]);
   const [idService, setIdService] = useState(0);
-
-  const [All, setAll] = useState([])
-  const [refresh, setRefresh] = useState(false)
+  const [formData, setFormData] = useState({
+    ca_name: '',
+    ca_img: null,
+  });
+  const [selectedNavItem, setSelectedNavItem] = useState("All services");
+  const [All, setAll] = useState([]);
+  const [refresh, setRefresh] = useState(false);
   const [newServiceName, setNewServiceName] = useState("");
   const [newServiceImage, setNewServiceImage] = useState("");
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    const form = new FormData();
+    form.append('file', file);
+    form.append('upload_preset', 'patient');
+
+    try {
+      const response = await axios.post(
+        'https://api.cloudinary.com/v1_1/dvr6fxw3p',
+        form,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
+
+      const imageUrl = response.data.secure_url;
+      setFormData((prev) => ({ ...prev, ca_img: imageUrl }));
+    } catch (err) {
+      console.log(err);
+    }
+  };
   useEffect(() => {
     axios.get(`http://localhost:3000/api/provider/type/${idService}`)
       .then(r => {
@@ -28,6 +59,7 @@ function DashboardOverview(props) {
         setSelectedprov(r.data)
       }).catch(err => console.log(err))
   }, [refresh, idService]);
+
   useEffect(() => {
     axios.get(`http://localhost:3000/api/service`)
       .then(r => {
@@ -39,41 +71,42 @@ function DashboardOverview(props) {
   useEffect(() => {
     axios.get(`http://localhost:3000/api/users`)
       .then(r => {
-        console.log('all', r.data)
+        console.log('prov', r.data)
         setSelecteduser(r.data)
       }).catch(err => console.log(err))
   }, [refresh])
+
   const handleServiceClick = (id) => {
-    setIdService(id)
+    setIdService(id);
   };
 
   const handleDeleteProv = async (id) => {
     try {
       await axios.delete(`http://localhost:3000/api/provider/${id}`);
       setRefresh(!refresh);
-
     } catch (err) {
       console.log(err);
     }
-  }
+  };
+
   const handleDeleteUser = async (id) => {
     try {
       await axios.delete(`http://localhost:3000/api/users/${id}`);
       setRefresh(!refresh);
-
     } catch (err) {
       console.log(err);
     }
-  }
+  };
+
   const handleDeleteService = async (id) => {
     try {
       await axios.delete(`http://localhost:3000/api/service/${id}`);
       setRefresh(!refresh);
-
     } catch (err) {
       console.log(err);
     }
-  }
+  };
+
   const handleAddService = async (e) => {
     e.preventDefault();
     try {
@@ -90,12 +123,17 @@ function DashboardOverview(props) {
       console.log(err);
     }
   };
+
   const handleServiceClick2 = (data) => {
     setSelectedData("All services");
   };
+
   const handleServiceClick3 = (user) => {
     setSelectedData("All users");
   };
+
+
+
   return (
     <div className="bg-stone-950 ">
       <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0" style={{ backgroundColor: 'black' }}>
@@ -111,30 +149,32 @@ function DashboardOverview(props) {
               className="aspect-[0.99] object-contain object-center w-[86px] overflow-hidden max-w-full self-start"
             />
             <div className="text-white text-3xl font-semibold leading-9 self-stretch mt-7">
-              Samantha
+              {Cookies.get('name', { expires: 60 * 60 * 24 })}
             </div>
             <div className="text-white text-lg leading-7 self-stretch whitespace-nowrap mt-5">
-              samantha@email.com
+              {Cookies.get('email', { expires: 60 * 60 * 24 })}
             </div>
             <div className="text-white text-2xl font-semibold leading-9 self-stretch mt-32 max-md:mt-10">
               Dashboard:
             </div>
-            <div className="text-white text-2xl font-semibold leading-9 self-stretch mt-12 max-md:mt-10"
-              onClick={handleServiceClick2}
-            >
-              All services
-            </div>
-            <div className="text-white text-2xl font-semibold leading-9 self-stretch mt-10"
-              onClick={handleServiceClick3}
-            >
-              All users
-            </div>
+            <div
+                  className={`text-white text-2xl font-semibold leading-9 self-stretch mt-12 max-md:mt-10 ${selectedNavItem === "All services" ? 'text-blue-500' : ''}`}
+                  onClick={handleServiceClick2}
+                >
+                  All services
+                </div>
+                <div
+                  className={`text-white text-2xl font-semibold leading-9 self-stretch mt-10 ${selectedNavItem === "All users" ? 'text-blue-500' : ''}`}
+                  onClick={handleServiceClick3}
+                >
+                  All users
+                </div>
             <div className="text-white text-2xl font-semibold leading-9 self-stretch mt-12 max-md:mt-10">
               Event request
             </div>
-            <div className="text-white text-2xl font-semibold leading-9 self-stretch mt-10 max-md:mt-10">
+            <Link to="/edit" className="text-white text-2xl font-semibold leading-9 self-stretch mt-10 max-md:mt-10">
               Accounts
-            </div>
+            </Link>
             <div className="text-white text-2xl font-semibold leading-9 self-stretch mt-11 max-md:mt-10">
               Settings
             </div>
@@ -163,17 +203,68 @@ function DashboardOverview(props) {
                     src="https://cdn.builder.io/api/v1/image/assets/TEMP/7828f9828a0665dcbbc753ef69b3be2ba7105914e9b9c58c0f22359bf7246524?"
                     className="aspect-[8.5] object-contain object-center w-[510px] overflow-hidden mt-12 max-md:max-w-full max-md:mt-10"
                   />
+
+
+
+
                   <div className="self-stretch mt-14 max-md:max-w-full max-md:mt-10">
                     <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
                       <div className="flex flex-col items-stretch w-[84%] max-md:w-full max-md:ml-0">
                         <span className="flex grow flex-col items-stretch max-md:mt-10">
+                          {selectedData === "All services" && (
+                            <div>
+                              <form onSubmit={handleAddService}>
+                                <label htmlFor="newServiceName" className="block text-sm font-medium text-gray-700">
+                                  Service Name
+                                </label>
+                                <input
+                                  type="text"
+                                  id="newServiceName"
+                                  name="newServiceName"
+                                  value={newServiceName}
+                                  onChange={(e) => setNewServiceName(e.target.value)}
+                                  className="mt-1 p-2 w-full border rounded-md"
+                                  required
+                                />
+                                <div className="input-div">
+        <label className="file-label">
+          <input
+            className="input"
+            name="picture"
+            type="file"
+            onChange={handleImageUpload}
+          />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            viewBox="0 0 24 24"
+            strokeWidth="2"
+            fill="none"
+            stroke="currentColor"
+            className="icon"
+          >
+            <polyline points="16 16 12 12 8 16"></polyline>
+            <line y2="21" x2="12" y1="12" x1="12"></line>
+            <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
+            <polyline points="16 16 12 12 8 16"></polyline>
+          </svg>
+        </label>
+      </div>
 
-
-                          {
-                            selectedData === "All services" && All.map((e, i) => (
-
+      <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full" style={{ backgroundColor: "#28608F" }}>
+        Add Service
+      </button>
+   
+                              
+                              </form>
+                            </div>
+                          )}
+                          {selectedData === "All services" &&
+                            All.map((e, i) => (
                               <div className="flex items-stretch justify-between gap-4 mt-10" key={i} onClick={() => handleServiceClick(e.id)}>
-
                                 <img
                                   loading="lazy"
                                   src={e.service_image}
@@ -183,15 +274,12 @@ function DashboardOverview(props) {
                                   <div className="text-slate-800 text-base font-medium tracking-wide">
                                     {e.service_name}
                                   </div>
-                                  
-
                                 </span>
-                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full" style={{backgroundColor:"#28608F"}} onClick={() => handleDeleteService(e.id)}>
-                                    Delete Service
-                                  </button>
+                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full" style={{ backgroundColor: "#28608F" }} onClick={() => handleDeleteService(e.id)}>
+                                  Delete Service
+                                </button>
                               </div>
                             ))}
-
                           {selectedData === "All users" && selecteduser.map((e, i) => (
                             <div className="flex items-stretch justify-between gap-4 mt-10" key={i} onClick={() => handleServiceClick(e.name)}>
                               <img
@@ -206,19 +294,24 @@ function DashboardOverview(props) {
                                 <div className="text-gray-700 text-sm tracking-wide whitespace-nowrap mt-2.5">
                                   {e.email}
                                 </div>
-                               
                               </span>
-                              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full" style={{backgroundColor:"#28608F"}} onClick={() => handleDeleteUser(e.id)}>
-                                  Delete Veterinarian
-                                </button >
+                              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full" style={{ backgroundColor: "#28608F" }} onClick={() => handleDeleteUser(e.id)}>
+                                Delete Veterinarian
+                              </button>
                             </div>
                           ))}
-
                         </span>
                       </div>
-
                     </div>
                   </div>
+
+
+
+
+
+
+
+
                 </div>
               </div>
               <div className="flex flex-col items-stretch w-[41%] ml-5 max-md:w-full max-md:ml-0" style={{ backgroundColor: '#F9FAFC' }}>
@@ -248,32 +341,13 @@ function DashboardOverview(props) {
                             {e.email}
                           </div>
                         </span>
-                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full" style={{backgroundColor:"#28608F"}} onClick={() => handleDeleteProv(e.id)}>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-2 rounded-full" style={{ backgroundColor: "#28608F" }} onClick={() => handleDeleteProv(e.id)}>
                           Delete Veterinarian
                         </button >
                       </span>
                     </div>
                   ))}
-                  {/* {selectedService === "pet sitter" &&
-                    petSitter.map((e, i) => (
-                      <div key={i} >
-                        <span className="flex items-stretch justify-between gap-5 mt-9">
-                          <img
-                            loading="lazy"
-                            src={e.image}
-                            className="aspect-square object-contain object-center w-12 overflow-hidden shrink-0 max-w-full"
-                          />
-                          <span className="flex grow basis-[0%] flex-col items-stretch mt-2 self-start">
-                            <div className="text-slate-800 text-base font-medium tracking-wide">
-                              {e.name}
-                            </div>
-                            <div className="text-gray-700 text-sm tracking-wide whitespace-nowrap mt-2.5">
-                              {e.email}
-                            </div>
-                          </span>
-                        </span>
-                      </div>
-                    ))} */}
+
 
 
 
@@ -284,50 +358,44 @@ function DashboardOverview(props) {
                     <div className="bg-emerald-400 flex shrink-0 h-[5px] flex-col rounded-md" />
                   </div>
 
-
+{/* 
                   <span className="bg-slate-100 flex w-full flex-col items-stretch mt-24 mb-2.5 pb-6 px-6 rounded-2xl max-md:mt-10 max-md:px-5">
+                    <div className="input-div">
+                      <label className="file-label">
+                        <input
+                          className="input"
+                          name="picture"
+                          type="file"
+                          onChange={handleImageUpload}
+                        />
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="1em"
+                          height="1em"
+                          strokeLinejoin="round"
+                          strokeLinecap="round"
+                          viewBox="0 0 24 24"
+                          strokeWidth="2"
+                          fill="none"
+                          stroke="currentColor"
+                          className="icon"
+                        >
+                          <polyline points="16 16 12 12 8 16"></polyline>
+                          <line y2="21" x2="12" y1="12" x1="12"></line>
+                          <path d="M20.39 18.39A5 5 0 0 0 18 9h-1.26A8 8 0 1 0 3 16.3"></path>
+                          <polyline points="16 16 12 12 8 16"></polyline>
+                        </svg>
+                      </label>
+                    </div>
+
+                  </span> */}
 
 
-                  </span>
                 </span>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="bg-white rounded-[30px] max-w-[400px] p-8 mt-8 mx-auto">
-        <h2 className="text-2xl font-semibold mb-4">Add New Service</h2>
-        <form onSubmit={handleAddService}>
-          <label htmlFor="newServiceName" className="block text-sm font-medium text-gray-700">
-            Service Name
-          </label>
-          <input
-            type="text"
-            id="newServiceName"
-            name="newServiceName"
-            value={newServiceName}
-            onChange={(e) => setNewServiceName(e.target.value)}
-            className="mt-1 p-2 w-full border rounded-md"
-            required
-          />
-
-          <label htmlFor="newServiceImage" className="block mt-4 text-sm font-medium text-gray-700">
-            Service Image URL
-          </label>
-          <input
-            type="text"
-            id="newServiceImage"
-            name="newServiceImage"
-            value={newServiceImage}
-            onChange={(e) => setNewServiceImage(e.target.value)}
-            className="mt-1 p-2 w-full border rounded-md"
-            required
-          />
-
-          <button type="submit" className="mt-4 p-2 bg-blue-500 text-black rounded-md cursor-pointer"  >
-            Add Service
-          </button>
-        </form>
       </div>
     </div>
   );
